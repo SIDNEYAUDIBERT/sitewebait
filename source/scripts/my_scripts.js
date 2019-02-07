ddadbe873f6d111a5d08a9510d97e9f73344c54f
sid <sidney.audibert@gmail.com>
2019-02-07 12:01:24 +0100

    add my_scripts.js

---
+++ source/scripts/my_scripts.js
@@ -0,0 +1,16 @@
+/*
+* init 
+* called when body loaded
+*/
+
+function init() {
+    document.getElementById("test").innerHTML = "init says Hi!";
+
+}
+
+function test_button() {
+    var my_test_div = document.getElementById("test");
+    my_test_div.innerHTML = "Je suis content !";
+    my_test_div.style.backgroundColor = "green"
+    alert("Clicked");
+}
