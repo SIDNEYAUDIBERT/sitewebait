1512f63ad94de25d77bdcb55489625c154f5d884
sid <sidney.audibert@gmail.com>
2019-02-14 09:51:33 +0100

    git commit

--- source/scripts/my_scripts.js
+++ source/scripts/my_scripts.js
@@ -8,16 +8,17 @@ function init() {
 
 }
 
-
-
-
-
 function test_button() {
     var my_test_div = document.getElementById("test");
     my_test_div.innerHTML = "Je suis content !";
     my_test_div.style.backgroundColor = "green"
     alert("Clicked");
+}
 
+function test() {
+    setInterval(myFunction, 1000);
+}
 
-
+function myFunction() {
+    console.log('COUCOU');
 }
